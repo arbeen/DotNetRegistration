48d8b08849b7ab168db0fcf25074adf451b25db4
iamarbin <arbinshrestha98@gmail.com>
2018-09-13 20:10:26 +0545

    Add project files.

---
+++ WebApplication1/Controllers/UsersController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class UsersController : Controller
+    {
+        // GET: Users
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ViewResult Login()
+        {
+            var arbin = new User() { FirstName = "arbin", LastName = "shrestha", Password = "arbinpass" };
+            
+            return View(arbin);
+        }
+
+        public ViewResult Register()
+        {
+            return View();
+        }
+    }
+}
